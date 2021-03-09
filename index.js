e47858da65ba8ded0b9376fa94df829fe7b21ef5
Nimisha Gupta <55681339+nimisha2001@users.noreply.github.com>
2021-03-09 21:12:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+//document.write("WOW");
+document.getElementById('head').style.color= 'white'
+document.getElementById('head').style.background= 'white'
+document.getElementById('header').style.outlineColor= 'black'
+document.getElementById('name').style.color= 'white'
+document.getElementById('1stbox').style.margin= "20";
+//<img id="1stbox" class="img2" src="VisualStudio_Images/1stbox_new.png" width=900>
+/* <video id="mainvideo" controls width="900">
+            <source src="VisualStudio_Images/InShot_20210306_001922120.mp4">
+        </video>  */
+document.getElementById('AboutMe').style.background= 'beige'
+document.getElementById('MyCourses').style.background= 'beige'
+document.getElementById('ContactMe').style.background= 'black'
+document.getElementById('cont').style.color= 'white'
+document.getElementById('Hobby').style.background= 'beige'
+document.getElementById('head5').style.background= 'black'
+
+
+window.onscroll = function() {          //keeping pos fixed on screen
+    header.style.top= 0;
+        //header.style.position= "fixed"; 
+    
+}
+
+
+
+//Lines in HTML
+// 13. background-attachment:local - keeping bg moving with scroll
+//p- para- 
+//a- header
+//h1- heading
+//h2- Hey and heading of boxes
+//h3- Margin change for box1 and container1
+//h4- Margin change for container2
+//body- to adjust bg 
+//img- 
